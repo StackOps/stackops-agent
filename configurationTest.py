4cff9b66c472bdd6a1dffede95456c83184a2538
diegoparrilla <diego.parrilla.santamaria@gmail.com>
2012-07-01 17:31:39 +0200

    check if database nova is created

--- configurationTest.py
+++ configurationTest.py
@@ -18,6 +18,7 @@ import configuration
 import StackOps
 import urllib
 import urllib2
+import utils
 
 
 from install import Filler
@@ -32,20 +33,43 @@ class MySQLConfigTest(unittest.TestCase):
         c =configuration.MySQLMasterConfig()
         c.mysql_root_password = 'stackops'
         result = c.installPackages()
-	self.assertTrue(result==None)
+    	self.assertTrue(result is None)
 
-#    def testConfigure(self):
+    def testConfigure(self):
 #        url = 'http://127.0.0.1:8888'
 #        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 #        values = {'sysinfo' : 'Michael Foord' }
 #        headers = { 'User-Agent' : user_agent }
-#
 #        data = urllib.urlencode(values)
 #        req = urllib2.Request(url, data, headers)
 #        response = urllib2.urlopen(req)
 #        the_page = response.read()
-#
-#        self.assertTrue(the_page)
+        c =configuration.MySQLMasterConfig()
+        c.mysql_root_password = 'stackops'
+        result = c.installPackages()
+
+        # NOVA database configuration
+        c.nova_username = "nova"
+        c.nova_password = "stackops"
+        c.nova_schema = "nova"
+        c.nova_drop_schema = True
+
+        # GLANCE database configuration
+        c.glance_username = "glance"
+        c.glance_password = "stackops"
+        c.glance_schema = "glance"
+        c.glance_drop_schema = True
+
+        # KEYSTONE database configuration
+        c.keystone_username = "keystone"
+        c.keystone_password = "stackops"
+        c.keystone_schema = "keystone"
+        c.keystone_drop_schema = True
+
+        c._configureMySQL()
+
+        (stdout, stderr) = utils.execute('mysql -uroot -p%s -e "SHOW DATABASES LIKE %s;"' % (self.mysql_root_password, self.nova_schema))
+        self.assertEqual(stdout,"nova")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
